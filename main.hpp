03be0dd8c14e9cb449aa726ab84356231d8464e7
jay <jayly.2506@gmail.com>
2025-04-25 19:48:23 -0700

    done

--- main.hpp
+++ main.hpp
@@ -1,3 +1,39 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int writeFile(const string &filename);
+int readFile(const string &filename);
+
+int writeFile(const string &filename) {
+    ofstream file(filename);
+    int empCount = 5; // Example number of employees
+
+    if (file.is_open()) {
+        for (int i = 0; i < empCount; ++i) {
+            file << "Employee " << i + 1 << endl;  // Writing dummy employee data
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file for writing" << endl;
+        return 0;
+    }
+    return empCount;
+}
+
+int readFile(const string &filename) {
+    ifstream file(filename);
+    int empCount = 0;
+    string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            empCount++;
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file for reading" << endl;
+    }
+    return empCount;
+}
